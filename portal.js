4df0ab63bdabe44660891a108efdd7eb982db49a
Collin Werner <collinw9898@gmail.com>
2017-12-16 23:23:39 -0500

    now able to get top posts via search

--- portal.js
+++ portal.js
@@ -3,23 +3,40 @@ module.exports = function(socket, reddit) {
     const tokenizer = require('./tokenizer')(reddit);
     const topicSearch = require('./topicSearch')(reddit); 
 
-    // find top words in a subreddit
+    // search via subreddit name
     socket.on('subreddit_name_update', (subRedditName) => {
         console.log('subreddit_name_update: ' + subRedditName);
 
+        // get most common words (recently used) in subreddit
         tokenizer.findTopWords(subRedditName, (words, freq) => {
             socket.emit('summarize_subreddit', {
                 topics: words,
                 frequency: freq
             })
         });
+
+        // get top posts (within last 24 hours) of subreddit
+        reddit.getSubreddit(subRedditName).getTop().map(post => post.title).then( (data) => {
+            socket.emit('getTitles', data);
+        });
     });
 
-    // find top subreddits based on word/phrase
+    // search via word/phrase
     socket.on('topic_search', (topic) => {
         console.log('topic_search: ' + topic);
+
+        // displays subreddits where word/phrase is most commonly used
         topicSearch.searchTopic(topic, (subredditHeatmap) => {
             socket.emit('topic_activity_by_subreddit', subredditHeatmap);
         });
+
+        // retrieves top posts about the word/phrase
+        reddit.search({
+            query: topic,
+            sort: 'top'
+        }).map(post => post.title)
+        .then( (data) => {
+            socket.emit('getTitlesByTopic', data);
+        }); 
     });
 }
